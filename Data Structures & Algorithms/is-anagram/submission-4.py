0d3307d0348c4502c2299d8286a3483ceb11fb11
oliverhisajifujiki <oliver.fujiki@gmail.com>
2026-04-05 06:03:01 -0700

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        sDict = {}
+        tDict = {}
+
+        for c in s:
+            if c not in sDict:
+                sDict[c] = 0
+            sDict[c] += 1
+        
+        for c in t:
+            if c not in tDict:
+                tDict[c] = 0
+            tDict[c] += 1
+        
+        if sDict == tDict:
+            return True
+        
+        return False
