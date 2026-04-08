ec630c8853f9a3e820d60d43aca5dd055d8702b0
oliverhisajifujiki <oliver.fujiki@gmail.com>
2026-04-08 08:34:33 -0700

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        sDict = {}
+        for c in s:
+            if c not in sDict:
+                sDict[c] = 0
+            sDict[c] += 1
+        
+        tDict = {}
+        for c in t:
+            if c not in tDict:
+                tDict[c] = 0
+
+            tDict[c] += 1
+        
+        return sDict == tDict
