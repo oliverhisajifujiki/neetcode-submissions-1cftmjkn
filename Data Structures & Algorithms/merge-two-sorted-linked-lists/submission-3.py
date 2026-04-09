42e24f76d6fdb3a68d590093b360012db8eb6f31
oliverhisajifujiki <oliver.fujiki@gmail.com>
2026-04-09 10:19:09 -0700

    Update: merge-two-sorted-linked-lists - submission-3

--- Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-3.py
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-3.py
@@ -5,12 +5,29 @@
 #         self.next = next
 
 class Solution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        cur = head
-        prev = None
-        while cur:
-            temp = cur.next
-            cur.next = prev
-            prev = cur
-            cur = temp
-        return prev
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        merged = ListNode() #init and a holder for the head
+        tail = merged #this is the variable we will use to add onto the merged
+
+        while list1 and list2: #go until one is empty 
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+        
+        #one or both of the lists are empty 
+        if list1 == None and list2 != None:
+            tail.next = list2
+        
+        elif list1 != None and list2 == None:
+            tail.next = list1
+        
+        #both lists are empty we are indeed done
+
+        merged = merged.next
+
+        return merged 
+
